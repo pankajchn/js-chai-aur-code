7c048750b645fa2328521b1b84c9c095aff7aeae
Pankaj Chouhan <161233018+pankajchn@users.noreply.github.com>
2024-04-27 16:37:36 +0000

    variables and data_types in JavaScript

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accoundId = 255645;
+let accountEmail = "theshantpreneur@gmail.com";
+var accountPassword = "12456";
+accountCity = "Jaipur";
+let accountState;
+
+//accoundId = 2;
+
+accountEmail = "realpankajchouhan007@gmail.com";
+accountPassword = "78555255";
+accountCity = "Phulera";
+
+console.table([accoundId, accountEmail, accountPassword, accountCity, accountState]);
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(8*9) //we are using nodejs not browser
+
+console.log(8*9), console.log("prashant")  //code readability should be high
+
+//Primitive Data Types
+
+//number
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => 
+//symbol => unique
+
+
+//object
+
+console.log(typeof undefined);
