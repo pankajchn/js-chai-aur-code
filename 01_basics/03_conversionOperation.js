b48f692a240bd2fd683c0133a68c232677311f6b
Pankaj Chouhan <161233018+pankajchn@users.noreply.github.com>
2024-04-28 10:45:20 +0000

    Type Conversion in JavaScript

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,51 @@
+// let score = 33;
+// console.log(typeof score);
+
+// let marks;
+// console.log(typeof marks);
+
+// let str = "";
+// console.log(typeof(str))
+
+// let batman = "89pkj"
+// console.log(typeof batman)
+
+// let superman = null
+// console.log(typeof superman)
+
+// let wonderWomen = undefined;
+// console.log(typeof wonderWomen)
+
+// let str = 89
+// let inBoolean = Boolean(str);
+// console.log(inBoolean)
+
+// let score = null;
+// console.log(typeof score)
+
+// let inNum = Number(score)
+// console.log(score)
+// console.log(inNum)
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+// let isLoggedIn = 0;
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
+
+    // 1(inBoolean)=> true;   
+    //  0(inBoolean) => false;
+    //  ""(inBoolean) => false;
+    // "pankaj"(inBoolean) => true;
+
+    let someNumber = 89;
+    console.log(typeof someNumber)
+    let stringNum = String(someNumber)
+    console.log(stringNum)
+    console.log(typeof stringNum)
+
+
+
+
