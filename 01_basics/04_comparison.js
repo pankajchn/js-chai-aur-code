63344e4faff663f237b8fe34b4d3d744e318e0bb
Pankaj Chouhan <161233018+pankajchn@users.noreply.github.com>
2024-04-28 17:17:47 +0000

    In this tutorial i learnt comparison operators and how to use them.

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,22 @@
+// console.log(2>1)
+// console.log(3<1)
+// console.log(2<=1)
+
+// console.log("2">1);
+// console.log("02">1);
+
+// console.log(null > 0);
+// console.log(null < 0);
+// console.log(null <= 0);
+// console.log(null >= 0);
+// console.log(null == 0);
+
+
+// ===
+// console.log("2" == 2);      //Output: true
+// console.log("2" === 2);    //Output: false
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
