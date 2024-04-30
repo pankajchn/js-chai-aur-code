95e461b32e19f0bb70fc49f16fa43e8fbf21a599
Pankaj Chouhan <161233018+pankajchn@users.noreply.github.com>
2024-04-30 16:23:35 +0000

    In this tutorial i learnt Math Object, its properties and its methds

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,25 @@
+// const score = 400;
+// console.log(score);
+
+// const balance = new Number(100);
+// console.log(balance);
+
+// const covertInString = balance.toString();
+// console.log(covertInString);
+// console.log(typeof(covertInString));
+
+// const fixedNumber = balance.toFixed(1);
+// console.log(fixedNumber);
+
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+// +++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+log
