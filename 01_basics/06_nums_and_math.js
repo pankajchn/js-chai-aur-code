0c9cf25e5275cce3fa959d520ce00af963718e04
Pankaj Chouhan <161233018+pankajchn@users.noreply.github.com>
2024-05-03 05:54:02 +0000

    Math Object and Number Method in JS

--- 01_basics/06_nums_and_math.js
+++ 01_basics/06_nums_and_math.js
@@ -11,15 +11,38 @@
 // const fixedNumber = balance.toFixed(1);
 // console.log(fixedNumber);
 
-// const otherNumber = 123.8966;
+///const otherNumber = 123.8966;
 // console.log(otherNumber.toPrecision(3));
 
-const hundreds = 1000000000000;
+//const hundreds = 1000000000000;
 // console.log(hundreds.toLocaleString('en-IN'));
-
 
 // +++++++++++++++++++++++++++++++++++++++++++ Maths +++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4));
-log
+// console.log(Math);
+// console.log(Math.abs(-4));
+// log
+
+// ----------------------------------------------------------------------------------------------------------------------
+
+// const otherNumber = 123.8966;
+// console.log(otherNumber.toPrecision(4));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+
+// const myNum = Number(undefined);
+// console.log(myNum)
+
+// const myNum1 = Number(null);
+// console.log(myNum1);
+
+// const myNum2 = null;
+// const myNum2InBoolean = Number(myNum2);
+// console.log(myNum2InBoolean);
+
+
+
+
+console.log(Math.random())
+console.log((Math.random()*10) + 1)
