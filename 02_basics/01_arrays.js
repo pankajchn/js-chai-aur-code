8340d9738bf3521bd8fb5e6f678a1cbbbe5827d8
Pankaj Chouhan <161233018+pankajchn@users.noreply.github.com>
2024-05-05 11:58:03 +0000

    In this tutorial i learnt Arrays and its Various Methods

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,21 @@
+const myArray = [0, 1, 2, 3, 4, 5];
+// console.log(myArray);
+// console.log(myArray[4]);
+
+// console.log(myArray.unshift(9));
+// console.log(myArray.unshift(7));
+
+// myArray.shift()
+
+// myArray.push(34)
+// myArray.pop()
+
+// console.log(myArray.includes(5));
+// console.log(myArray.indexOf(0));
+
+// const newArray = myArray.join();
+// console.log(myArray);
+// console.log(typeof(myArray));
+// console.log(newArray);
+
+// Slice and Splice
